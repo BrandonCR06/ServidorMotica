e76a7a162c7c90dd5bd56e61eb53ff3867c160d2
Ricardo Soto <ricardosoto@MacBook-Air-de-Ricardo.local>
2022-10-12 07:26:36 -0600

    Registrar preguntas y retornar preguntas

--- record.js
+++ record.js
@@ -1,5 +1,4 @@
 const express = require("express");
- 
 // recordRoutes is an instance of the express router.
 // We use it to define our routes.
 // The router will be added as a middleware and will take control of requests starting with path /record.
@@ -49,7 +48,35 @@ recordRoutes.get('/products', (req, res) =>{
   });
   
 });
+/** var ObjectId = require('mongodb').ObjectId; 
+var id = req.params.gonderi_id;       
+var o_id = new ObjectId(id);
+db.test.find({_id:o_id})
+*/ 
+recordRoutes.get('/get/preguntas', (req, res) =>{
+  dbo.connection.useDb('MoticaDB').collection("Preguntas").aggregate(
+    [{$lookup:{from :"Users",localField:"usuario",foreignField:"_id", as: "usuario"}}])
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
 
+}
+);
+recordRoutes.post('/register/question', (req, res) =>{
+  var userid = req.body.usuario;
+  var o_id = new ObjectId(userid);  
+  let myobj = {
+      contenido:req.body.contenido,        
+      usuario: o_id,
+      respuesta:  req.body.respuesta
+    };
+    console.log(req.body);
+  dbo.connection.useDb('MoticaDB').collection("Preguntas").insertOne(myobj, function (err, result) {
+      if (err) console.log (err);
+      res.json(result);
+    });
+});
 
 recordRoutes.post('/add/user', (req, res) =>{
     let myobj = {
